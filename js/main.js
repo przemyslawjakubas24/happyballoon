e564c7fd624d6dad390fef64eda7bb4185911ca2
przemyslawjakubas24 <przemyslawjakubas24@gmail.com>
2024-10-14 00:16:34 +0200

    test5

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ burgerBtn.addEventListener('click', () => {
 })
 
 function handleScroll() {
-	if (window.scrollY >= 250) {
+	if (window.scrollY >= 200) {
 		sizeNav.style.padding = '1em 2em'
 		sizeNavLogo.style.width = '40px'
 	} else {
